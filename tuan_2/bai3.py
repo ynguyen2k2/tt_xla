89393ad5fb51e8956e24c68c8b85a4fac6c11e0c
ynguyen2k2 <ynguyen2017tt@gmail.com>
2023-04-07 00:35:29 +0700

    commit

--- tuan_2/bai3.py
+++ tuan_2/bai3.py
@@ -1,4 +1,7 @@
 
 
-import matplotlib.pyplot as plt
-from skimage import color,data
+from matplotlib import pyplot as plt
+import numpy as np
+
+path=r'D:\Desktop\tt_xla\git\tt_xla\tuan_2\image\vo.jpg'
+image=plt.imread(path)
